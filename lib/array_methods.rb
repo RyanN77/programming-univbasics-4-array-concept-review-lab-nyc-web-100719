73eab976a8b4c782d0a275a74d692d9cb6d1ba07
adventurous-class-0665 <rngao97@gmail.com>
2019-09-16 20:47:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   num = 0
   while num < array.length
-    if num = 2
-    puts num
-    end
+    if array[num] = value_to_find
+    puts array[num]
+    break
     num += 1
   end
 end
